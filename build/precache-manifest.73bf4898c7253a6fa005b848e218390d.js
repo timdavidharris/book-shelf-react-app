092b5bff3764abac970caa3b2ddbd218f10064be
Tim David Harris <timdavidharris@gmail.com>
2022-02-06 17:30:23 -0700

    Ran - npm run build

---
+++ build/precache-manifest.73bf4898c7253a6fa005b848e218390d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58cf03731dab38ef7b5f762b600772a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9edf080f09f273364cf6",
+    "url": "/static/css/main.b471d61c.chunk.css"
+  },
+  {
+    "revision": "8a7a42481d35431d8685",
+    "url": "/static/js/2.a33ebee9.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.a33ebee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9edf080f09f273364cf6",
+    "url": "/static/js/main.759059ba.chunk.js"
+  },
+  {
+    "revision": "8346c29f16a289bb06e3",
+    "url": "/static/js/runtime-main.13ef3a12.js"
+  }
+]);
