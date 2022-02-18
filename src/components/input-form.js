eaa4d7a3c4205ea564f7d96b80019b7d8a7f542a
Tim David Harris <timdavidharris@gmail.com>
2022-02-18 15:44:23 -0700

    LocalStorage is now working

--- src/components/input-form.js
+++ src/components/input-form.js
@@ -6,7 +6,7 @@ class Form extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            library: localStorage.getItem('libraryArray') === null || localStorage.getItem('libraryArray') === undefined ? [this.setExampleBook_Dune(), this.setExampleBook_1984()] && this.updateLocalStorage() : JSON.parse(localStorage.getItem('libraryArray')),
+            library: localStorage.getItem('libraryArray') === null ? [this.setExampleBook_Dune(), this.setExampleBook_1984()] : JSON.parse(localStorage.getItem('libraryArray')),
             displayForm: false,
             bookTitle: '',
             bookAuthor: '',
@@ -101,7 +101,7 @@ class Form extends React.Component {
     }
 
     updateLocalStorage = () => {
-        return localStorage.getItem('libraryArray') === null || localStorage.getItem('libraryArray') === undefined ? null : localStorage.setItem('libraryArray', JSON.stringify(this.state.library));
+        localStorage.setItem('libraryArray', JSON.stringify(this.state.library));
     }
 
     // consoleLogLibrary = () => {
