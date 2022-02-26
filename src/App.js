9f2877e0b5d18052b7f1cce1d736ce2dbbf9383f
Tim David Harris <timdavidharris@gmail.com>
2022-02-25 18:33:08 -0700

    Adding comonent with GitHub link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/input-form';
+import GitHubLink from './components/github-link';
 
 class App extends React.Component {
   render() {
@@ -13,6 +14,7 @@ class App extends React.Component {
         </h2>
         <section id='book-cards'>
           <Form />
+          <GitHubLink />
         </section>
       </div>
     )
---
+++ src/components/github-link.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function GitHubLink() {
+    return (
+        <div>
+            <a href='https://github.com/timdavidharris/book-shelf-react-app' target='_blank' rel="noopener noreferrer">
+                GitHub Link
+            </a>
+        </div>
+    )
+}
+
+export default GitHubLink;
