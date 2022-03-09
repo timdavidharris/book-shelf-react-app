dcd5a71b85ab785d6db90a449260d52d6d2be500
Tim David Harris <timdavidharris@gmail.com>
2022-03-08 20:43:54 -0700

    Exported supabase on line 7

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,3 +3,5 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://viyyqfksopapspnrgxwi.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
