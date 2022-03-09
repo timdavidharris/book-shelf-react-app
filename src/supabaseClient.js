a76088671ec31e04100e2648958eb100e9968fd2
Tim David Harris <timdavidharris@gmail.com>
2022-03-08 20:33:31 -0700

    Setting up supabase with my specific api data

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabaseUrl = 'https://viyyqfksopapspnrgxwi.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
