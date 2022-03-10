505400159c3783960e952d6bd97f16e3da2388c5
Tim David Harris <timdavidharris@gmail.com>
2022-03-10 07:43:05 -0700

    Fixed the data select and insert fns

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -7,17 +7,19 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 function SupabaseComponent(props) {
     const library = props.library;
-    select().then(updateRow(library));
+    let newArray = library.map((book) => { return {'data': book} });
+    select();
+    insertRow(newArray);
     return(null);
 }
 
 async function select() {
-    let { data: test_table, error } = await supabase.from('test_table').select('inserted_at').select('updated_at').select('data');
+    let { data: test_table, error} = await supabase.from('test_table').select('*');
     return(null);
 }
 
-async function updateRow(library) {
-    const { data, error } = await supabase.from('test_table').update(library);
+async function insertRow(library) {
+    const{ data, error } = await supabase.from('test_table').insert(library);
     return(null);
 }
 
