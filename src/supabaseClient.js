8026325c5f1588d1e58aa0504288aea23164801f
Tim David Harris <timdavidharris@gmail.com>
2022-03-22 20:56:48 -0600

    Changed export location to match other files

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -5,7 +5,7 @@ const supabaseUrl = 'https://viyyqfksopapspnrgxwi.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpeXlxZmtzb3BhcHNwbnJneHdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDY3MDk5MTIsImV4cCI6MTk2MjI4NTkxMn0.HkJJK2a8rUPa-EU3VUeiibZD76AJSjTK7rQ7BcWi_as';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-function SupabaseComponent(props) {
+export default function SupabaseComponent(props) {
     const library = props.library;
     let newArray = library.map((book) => { return {'data': book} });
     select();
@@ -22,5 +22,3 @@ async function insertRow(library) {
     const{ data, error } = await supabase.from('test_table').insert(library);
     return(null);
 }
-
-export default SupabaseComponent;
