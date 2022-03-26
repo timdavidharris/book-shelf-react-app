3121bb9479958d12ef145c0a65b4c9d08ced203d
Tim David Harris <timdavidharris@gmail.com>
2022-03-25 19:37:39 -0600

    Refactored App.js to just return Library compnt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import React from 'react';
-import AddBook from './components/AddBook';
-import GithubLink from './components/GithubLink';
+import Library from './components/Library';
 
-class App extends React.Component {
-  render() {
-    return(
-      <div>
-        <h1>
-          Book Shelf
-        </h1>
-        <h2>
-          Books on Your Shelf
-        </h2>
-        <section id='book-cards'>
-          <AddBook />
-          <GithubLink />
-        </section>
-      </div>
-    )
-  }
+export default function App() {
+  return (<Library />);
 }
-
-export default App;
