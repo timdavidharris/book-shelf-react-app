6448c9ed9d373bc1804e7fc6d62fe05ba570d522
Tim David Harris <timdavidharris@gmail.com>
2022-03-25 19:52:10 -0600

    Added library array and moves async fns

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-import React from 'react'
+import React, { useState } from 'react'
 import AddBook from './components/AddBook';
 import Book from './components/Book';
 import GithubLink from './GithubLink';
@@ -8,20 +8,36 @@ const supabaseUrl = 'https://viyyqfksopapspnrgxwi.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpeXlxZmtzb3BhcHNwbnJneHdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDY3MDk5MTIsImV4cCI6MTk2MjI4NTkxMn0.HkJJK2a8rUPa-EU3VUeiibZD76AJSjTK7rQ7BcWi_as';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default function SupabaseComponent(props) {
-    const library = props.library;
+export default function SupabaseComponent() {
+    const [library, setLibrary] = useState([]);
+
+    async function upsertRow(library) {
+        library.forEach(async function(book) {
+            const newBook = {'data': book};
+            console.log(newBook);
+            const { data, error } = await supabase
+                .from('test_table')
+                .upsert({ id: newBook.data.id, book: newBook })
+        });
+    }
+    
+    async function readRows() {
+        let data = await supabase.from('test_table').select('*');
+        console.log(data);
+    }
+
     return(
         <main>
             <header>
                 <h1>
-                Book Shelf
+                    Book Shelf
                 </h1>
                 <h2>
-                Books on Your Shelf
+                    Books on Your Shelf
                 </h2>
             </header>
             <div>
-                <button onClick={() => insertRow(library)}>
+                <button onClick={() => upsertRow(library)}>
                     Click to Upsert Data
                 </button>
                 <button onClick={() => readRows()}>
@@ -41,17 +57,3 @@ export default function SupabaseComponent(props) {
     );
 }
 
-async function insertRow(library) {
-    library.forEach(async function(book) {
-        const newBook = {'data': book};
-        console.log(newBook);
-        const { data, error } = await supabase
-            .from('test_table')
-            .upsert({ id: newBook.data.id, book: newBook })
-    });
-}
-
-async function readRows() {
-    let data = await supabase.from('test_table').select('*');
-    console.log(data);
-}
