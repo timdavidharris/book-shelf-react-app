201d53d9ff544e5af346a3044ce74778a2c65860
Tim David Harris <timdavidharris@gmail.com>
2022-03-25 19:53:31 -0600

    Renamed functional component

--- src/components/Library.js
+++ src/components/Library.js
@@ -8,7 +8,7 @@ const supabaseUrl = 'https://viyyqfksopapspnrgxwi.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpeXlxZmtzb3BhcHNwbnJneHdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDY3MDk5MTIsImV4cCI6MTk2MjI4NTkxMn0.HkJJK2a8rUPa-EU3VUeiibZD76AJSjTK7rQ7BcWi_as';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default function SupabaseComponent() {
+export default function Library() {
     const [library, setLibrary] = useState([]);
 
     async function upsertRow(library) {
