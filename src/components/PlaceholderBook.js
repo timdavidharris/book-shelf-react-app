7ce44d46655eea2921bb0a6f57af2f187bfd270e
Tim David Harris <timdavidharris@gmail.com>
2022-03-31 12:41:15 -0600

    Added PlaceholderBook component

---
+++ src/components/PlaceholderBook.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function PlaceholderBook() {
+    return (
+        <div className='book-child-div'>
+            <span role='img' aria-label='emoji finger pointing up'>
+                ☝️
+            </span>
+            <h3>Humm... no books?</h3>
+            <h3>Click "Add A Book" to start your library!</h3>
+        </div>
+    )
+}
