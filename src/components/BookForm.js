720b7698e991ee5fb25b0b1cb3f11fc67bac6133
Tim David Harris <timdavidharris@gmail.com>
2022-04-01 21:25:07 -0600

    Formatted line 29 into a single line

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -26,13 +26,7 @@ export default function BookForm(props) {
             </label>
             )
         } else {
-            return (
-                <div>
-                    <label>
-                        Edit Book
-                    </label>
-                </div>
-            )
+            return (<div><label>Edit Book</label></div>)
         }
     }
 
